fde728f993f4cd5e3fb71dca77542d7e4da54c33
Administrator <Administrator@DESKTOP-PGKHSCB>
2019-02-18 09:50:33 +0700

    upgit

---
+++ CMCProjectMock1Team3/src/main/java/cmc/global/cms/CmcProjectMock1Team3Application.java
@@ -0,0 +1,13 @@
+package cmc.global.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmcProjectMock1Team3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmcProjectMock1Team3Application.class, args);
+	}
+
+}
